864118229aea6e42daca7cf755afa9242c1b3492
kiattisak jomram <t.kiattisak.j@gmail.com>
2025-04-25 20:37:02 +0700

    🎉 feat(auth): implement JWT authentication with login and user retrieval functionality

--- backend/src/modules/auth/auth.controller.ts
+++ backend/src/modules/auth/auth.controller.ts
@@ -1,6 +1,14 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Request,
+  UseGuards,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LoginDto } from './dto/login.dto';
+import { JwtAuthGuard } from './jwt-auth.guard';
 
 @Controller('auth')
 export class AuthController {
@@ -10,5 +18,12 @@ export class AuthController {
   async login(@Body() dto: LoginDto) {
     const user = await this.authService.loginOrRegister(dto.username);
     return { user };
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('me')
+  getMe(@Request() req) {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-return, @typescript-eslint/no-unsafe-member-access
+    return req.user;
   }
 }
--- backend/src/modules/auth/auth.service.ts
+++ backend/src/modules/auth/auth.service.ts
@@ -1,15 +1,23 @@
 import { PrismaService } from '@/infrastructure/prisma/prisma.service';
 import { Injectable } from '@nestjs/common';
-import { User } from '@prisma/client';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  constructor(private prisma: PrismaService) {}
+  constructor(
+    private prisma: PrismaService,
+    private jwtService: JwtService,
+  ) {}
 
-  async loginOrRegister(username: string): Promise<User> {
-    const existing = await this.prisma.user.findUnique({ where: { username } });
-    if (existing) return existing;
+  async loginOrRegister(username: string) {
+    let user = await this.prisma.user.findUnique({ where: { username } });
+    if (!user) {
+      user = await this.prisma.user.create({ data: { username } });
+    }
 
-    return this.prisma.user.create({ data: { username } });
+    const payload = { username: user.username, sub: user.id };
+    const token = this.jwtService.sign(payload);
+
+    return { user, token };
   }
 }
