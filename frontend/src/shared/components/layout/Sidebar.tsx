a54eca249ed3f4f6af2e36981fa66e642f081025
kiattisak jomram <t.kiattisak.j@gmail.com>
2025-04-26 15:16:46 +0700

    ✨ feat: add Drawer component from vaul and integrate into Topbar with Sidebar

--- frontend/src/shared/components/layout/Sidebar.tsx
+++ frontend/src/shared/components/layout/Sidebar.tsx
@@ -3,11 +3,9 @@ import Link from "next/link"
 
 export default function Sidebar() {
   return (
-    <aside className='flex flex-col w-full bg-background p-6 gap-6'>
-      <h1 className='text-xl font-serif italic text-popover-foreground'>
-        a Board
-      </h1>
-      <nav className='flex flex-col gap-4 text-popover-foreground'>
+    <aside className='flex flex-col w-full bg-transparent p-6 gap-6'>
+      <h1 className='text-xl font-serif italic text-inherit'>a Board</h1>
+      <nav className='flex flex-col gap-4 text-inherit'>
         <Link href='/' className='flex items-center gap-2 font-medium'>
           <Home size={20} /> Home
         </Link>
--- frontend/src/shared/components/layout/Topbar.tsx
+++ frontend/src/shared/components/layout/Topbar.tsx
@@ -3,6 +3,17 @@ import Link from "next/link"
 import { Button } from "../ui/button"
 import { useSession } from "next-auth/react"
 import { signOut } from "next-auth/react"
+import { ArrowRight, MenuIcon } from "lucide-react"
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "../ui/drawer"
+import Sidebar from "./Sidebar"
 
 export default function Topbar() {
   const session = useSession()
@@ -10,28 +21,47 @@ export default function Topbar() {
   return (
     <header className='w-full h-16 bg-sidebar-primary text-white px-6 py-4 flex items-center justify-between'>
       <div className='text-lg font-serif italic'>a Board</div>
-      {session.status !== "loading" && (
-        <div className='flex gap-4 items-center'>
-          {session.status === "authenticated" ? (
-            <>
-              <div>{session.data.user.name}</div>
+      <div className='flex items-center space-x-2'>
+        {session.status !== "loading" && (
+          <div className='flex gap-4 items-center'>
+            {session.status === "authenticated" ? (
+              <>
+                <div>{session.data.user.name}</div>
+                <Button
+                  onClick={() => signOut({ callbackUrl: "/login" })}
+                  className='bg-white text-sidebar-foreground px-4 py-1 rounded font-semibold'
+                >
+                  Sign Out
+                </Button>
+              </>
+            ) : (
               <Button
-                onClick={() => signOut({ callbackUrl: "/login" })}
+                asChild
                 className='bg-white text-sidebar-foreground px-4 py-1 rounded font-semibold'
               >
-                Sign Out
+                <Link href='/login'>Sign In</Link>
               </Button>
-            </>
-          ) : (
-            <Button
-              asChild
-              className='bg-white text-sidebar-foreground px-4 py-1 rounded font-semibold'
-            >
-              <Link href='/login'>Sign In</Link>
-            </Button>
-          )}
-        </div>
-      )}
+            )}
+          </div>
+        )}
+
+        <Drawer direction='right'>
+          <DrawerTrigger asChild>
+            <MenuIcon />
+          </DrawerTrigger>
+          <DrawerContent className='bg-foreground text-white'>
+            <DrawerHeader>
+              <DrawerTitle>
+                <DrawerClose asChild>
+                  <ArrowRight className='text-white' />
+                </DrawerClose>
+              </DrawerTitle>
+              <DrawerDescription />
+            </DrawerHeader>
+            <Sidebar />
+          </DrawerContent>
+        </Drawer>
+      </div>
     </header>
   )
 }
