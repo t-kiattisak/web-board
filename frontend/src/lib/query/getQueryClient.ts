f752ac4cdabbb6522cbc9787d66ff6147c48bc53
kiattisak jomram <t.kiattisak.j@gmail.com>
2025-04-26 15:03:10 +0700

    ✨ feat: update input color in globals.css and add error handling in query client

--- frontend/src/lib/query/getQueryClient.ts
+++ frontend/src/lib/query/getQueryClient.ts
@@ -1,11 +1,20 @@
 import { QueryClient } from "@tanstack/react-query"
 import { isServer, defaultShouldDehydrateQuery } from "@tanstack/react-query"
+import { isAxiosError } from "axios"
+import { toast } from "sonner"
 
 function makeQueryClient() {
   return new QueryClient({
     defaultOptions: {
       queries: {
         staleTime: 60 * 1000,
+      },
+      mutations: {
+        onError(error) {
+          if (isAxiosError(error)) {
+            toast(error.response?.data.message)
+          }
+        },
       },
       dehydrate: {
         shouldDehydrateQuery: (query) =>
--- frontend/src/lib/utils/network.ts
+++ frontend/src/lib/utils/network.ts
@@ -2,4 +2,7 @@ import axios from "axios"
 
 export const network = axios.create({
   baseURL: process.env.API_URL,
+  headers: {
+    Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImpvaG5fZG9lIiwic3ViIjoiODBjMmM3N2YtY2UxOC00ZmQzLTgxZDgtZjI4NWQ4MTc0ZGFkIiwiaWF0IjoxNzQ1NjUyNDYxLCJleHAiOjE3NDU2NTYwNjF9.OZyQVRO-RfdtlXzx1iclCt11vvp_imXIb3XFjokLGC4`,
+  },
 })
